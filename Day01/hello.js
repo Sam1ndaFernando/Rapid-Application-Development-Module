e9d9c8bf9a4de08dee8c0b2ffc740a2d3c880d4f
Saami <uthpala2001@gmail.com>
2024-11-10 19:42:21 +0530

    push

---
+++ Day01/hello.js
@@ -0,0 +1,5 @@
+var num = 10;
+console.log("Hello TS");
+// npm install -g typescript
+// tsc --init
+// tsc hello.ts
---
+++ Day01/hello.ts
@@ -0,0 +1,7 @@
+let num = 10;
+console.log("Hello TS");
+
+
+// npm install -g typescript
+// tsc --init
+// tsc hello.ts
